73eb2251c7c671c47832454246e2ee3bdc0ac83a
anithaganesan1 <aniganesan86@gmail.com>
2025-06-09 16:16:57 +0530

    all new changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App!</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
